8d235979e70560fc9c3086de3fba1675602955ee
Kareena2070 <kareenayadav1088@gmail.com>
2025-11-16 17:43:42 +0530

    Add Agency page with GSAP ScrollTrigger, pinned image container, and dynamic image swapping based on scroll progress

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -1,10 +1,72 @@
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
 function Agency() {
+  const imageDiveRef = useRef(null);
+  const imgaeRef = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+
+  const imageArray = [
+    "https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7",
+    "https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64",
+    "https://k72.ca/images/teamMembers/Lawrence_480x640.jpg?w=480&h=640&fit=crop&s=0a878205586092164001a9afe0ef4007",
+    "https://k72.ca/images/teamMembers/HugoJoseph_480x640.jpg?w=480&h=640&fit=crop&s=f152025b8a59b062d1e7978b5d6544c3",
+    "https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991",
+    "https://k72.ca/images/teamMembers/SophieA_480x640.jpg?w=480&h=640&fit=crop&s=fcb556060c29623e706dfbc4eeca87ac",
+    "https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2",
+    "https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6",
+    "https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a",
+    "https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847",
+    "https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6",
+  ];
+
+  useGSAP(function(){
+    gsap.to(imageDiveRef.current, {
+      scrollTrigger:{
+        trigger: imageDiveRef.current,
+        // markers:true,
+        start: "top 25%%",
+        end: "top -100% ",
+        scrub: true,
+        pin: true,
+        onUpdate: function(elem){
+           let imageIndex;
+         if(elem.progress <1){
+          imageIndex =  Math.floor(elem.progress * (imageArray.length));
+         } else{
+          imageIndex = imageArray.length -1;
+         }
+          imgaeRef.current.src = imageArray[imageIndex]
+        }
+      }
+    })
+  })
+
   return (
-    <div>
-     
-    </div>
+    <>
+      <div className="section1">
+        <div ref={imageDiveRef} className="absolute top-60 left-[25vw] h-[20vw] w-[15vw] rounded-4xl overflow-hidden">
+          <img ref={imgaeRef} className="h-full w-full object-cover" src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+        </div>
+
+        <div className="relative font-[font2]">
+          <div className=" mt-[55vh]">
+            <h1 className="text-[20vw] text-center uppercase leading-[18vw]">SEVEN7Y <br />
+              TWO</h1>
+          </div>
+
+          <div className=" relative pl-[35%] mt-0">
+            <p className="text-5xl ">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; We're inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="section2 h-screen"></div> */}
+    </>
+
   );
 }
 
