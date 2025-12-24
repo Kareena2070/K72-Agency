72dd70e49495c17a80ab731e43d41db79924afa0
Kareena2070 <kareenayadav1088@gmail.com>
2025-12-24 23:45:33 +0530

    add full-screen navigation overlay with animated links and image scroller

---
+++ src/components/Navigation/fullScreenNav.jsx
@@ -0,0 +1,99 @@
+
+function FullScreenNav() {
+    return (
+        <>
+            <div className="h-screen  w-full absolute bg-black text-white overflow-hidden ">
+
+                <div className="flex w-full justify-between items-start p-5">
+                    <div className="w-36 ">
+                        <svg className="w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 44" >
+                            <path fill="white" fillRule="evenodd" d="M35.1441047,8.4486911 L58.6905011,8.4486911 L58.6905011,-1.3094819e-14 L35.1441047,-1.3094819e-14 L35.1441047,8.4486911 Z M20.0019577,0.000230366492 L8.83414254,25.3433089 L18.4876971,25.3433089 L29.5733875,0.000230366492 L20.0019577,0.000230366492 Z M72.5255345,0.000691099476 L72.5255345,8.44846073 L94.3991559,8.44846073 L94.3991559,16.8932356 L72.5275991,16.8932356 L72.5275991,19.5237906 L72.5255345,19.5237906 L72.5255345,43.9274346 L102.80937,43.9274346 L102.80937,35.4798953 L80.9357483,35.4798953 L80.9357483,25.3437696 L94.3996147,25.3428482 L94.3996147,16.8953089 L102.80937,16.8953089 L102.80937,0.000691099476 L72.5255345,0.000691099476 Z M-1.30398043e-14,43.9278953 L8.78642762,43.9278953 L8.78642762,0.0057591623 L-1.30398043e-14,0.0057591623 L-1.30398043e-14,43.9278953 Z M58.6849955,8.4486911 L43.1186904,43.9274346 L52.3166592,43.9274346 L67.9877996,8.4486911 L58.6849955,8.4486911 Z M18.4688864,25.3437696 L26.7045278,43.9278953 L36.2761871,43.9278953 L28.1676325,25.3375497 L18.4688864,25.3437696 Z"></path>
+                        </svg>
+                    </div>
+
+                    <div className="menuCut h-30 w-20 relative">
+                        <div className="menuCut1 h-full w-1 absolute bg-white rotate-45 origin-right"></div>
+                        <div className="menuCut1 h-full w-1 absolute bg-white -rotate-45 origin-left"></div>
+                    </div>
+                </div>
+
+                <div className="py-10">
+                    <div className="link-ani border-t-1 relative ">
+                        <h1 className="font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Projects</h1>
+                        <div className="moveLink absolute flex bg-[#D3FD50] text-black top-0">
+                            <div className="moveX flex items-center">
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">SEE EVERYTHINGS</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="Image1" />
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">SEE EVERYTHINGS</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=1280&h=960&s=008dc81d7a1c8fa45e299f6d2dbafc93" alt="Image2" />
+                            </div>
+                            <div className="moveX flex items-center">
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">SEE EVERYTHINGS</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="Image1" />
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">SEE EVERYTHINGS</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=1280&h=960&s=008dc81d7a1c8fa45e299f6d2dbafc93" alt="Image2" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="link-ani border-t-1 relative ">
+                        <h1 className="font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">AGENCE</h1>
+                        <div className="moveLink absolute flex bg-[#D3FD50] text-black top-0">
+                            <div className="moveX flex items-center">
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Known us</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/teamMembers/PLP_640x280.jpg?w=640&h=290&s=e675a180832a690a53a22b25ea7fa365" alt="Image1" />
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Known us</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="Image2" />
+                            </div>
+                            <div className="moveX flex items-center">
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Known us</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/teamMembers/PLP_640x280.jpg?w=640&h=290&s=e675a180832a690a53a22b25ea7fa365" alt="Image1" />
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Known us</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="Image2" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="link-ani border-t-1 relative ">
+                        <h1 className="font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Contact</h1>
+                        <div className="moveLink absolute flex bg-[#D3FD50] text-black top-0">
+                            <div className="moveX flex items-center">
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Send us fax</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/blog/blogImg/50ff59cc0550df5b36543807a58db98c52e01a22274a317eafbfa5266941579b.png?w=640&h=290&s=4f8134f04fe18db7382b99cec63c95f5" alt="Image1" />
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Send us fax</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/caseStudies/Capture_d%E2%80%99ecran_le_2025-09-17_a_18.11.25.png?w=1920&fm=jpg&s=53fb69332aedd6c1fa5499623c55789a" alt="Image2" />
+                            </div>
+                            <div className="moveX flex items-center">
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Send us fax</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/blog/blogImg/50ff59cc0550df5b36543807a58db98c52e01a22274a317eafbfa5266941579b.png?w=640&h=290&s=4f8134f04fe18db7382b99cec63c95f5" alt="Image1" />
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Send us fax</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/caseStudies/Capture_d%E2%80%99ecran_le_2025-09-17_a_18.11.25.png?w=1920&fm=jpg&s=53fb69332aedd6c1fa5499623c55789a" alt="Image2" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="link-ani border-y-1 relative ">
+                        <h1 className="font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Blog</h1>
+                        <div className="moveLink absolute flex bg-[#D3FD50] text-black top-0">
+                            <div className="moveX flex items-center">
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Read articles</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/blog/blogImg/K72_article_ChatGPT_blogue.jpg?w=640&h=290&s=cec2aa341c22369e36e602c558c49e2a" alt="Image1" />
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Read articles</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=1280&h=960&s=008dc81d7a1c8fa45e299f6d2dbafc93" alt="Image2" />
+                            </div>
+                            <div className="moveX flex items-center">
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Read articles</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/blog/blogImg/K72_article_ChatGPT_blogue.jpg?w=640&h=290&s=cec2aa341c22369e36e602c558c49e2a" alt="Image1" />
+                                <h2 className="whitespace-nowrap font-[font2] text-[8vw] uppercase leading-[0.8] pt-10 text-center">Read articles</h2>
+                                <img className="shrink-0 w-70 h-36 rounded-full" src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=1280&h=960&s=008dc81d7a1c8fa45e299f6d2dbafc93" alt="Image2" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default FullScreenNav
